2113c34b8a39620220c07eb8d75f3d37a9f50583
Taewoon Kim <tae898@gmail.com>
2024-03-01 11:32:21 +0200

    make it work for low small rollout

--- AgentRoomEnv1/agent/ppo.py
+++ AgentRoomEnv1/agent/ppo.py
@@ -79,25 +79,20 @@ class PPOAgent(HandcraftedAgent):
         """Initialization.
 
         Args:
-            env_str: This has to be "room_env:RoomEnv-v1"
-            env_config: The configuration of the environment.
-            num_episodes: The number of iterations to train the agent.
-            replay_buffer_size: The size of the replay buffer.
-            batch_size: The batch size for training This is the amount of samples
+            env_str: This has to be "room_env:RoomEnv-v1" env_config: The configuration
+            of the environment. num_episodes: The number of iterations to train the
+            agent. replay_buffer_size: The size of the replay buffer. batch_size: The
+            batch size for training This is the amount of samples
                 sampled from the replay buffer.
-            max_epsilon: The maximum epsilon.
-            min_epsilon: The minimum epsilon.
-            gamma: The discount factor.
-            capacity: The capacity of each human-like memory systems.
-            pretrain_semantic: Whether or not to pretrain the semantic memory system.
-            nn_params: The parameters for the function approximator.
-            run_test: Whether or not to run test.
-            num_samples_for_results: The number of samples to validate / test the agent.
-            plotting_interval: The interval to plot the results.
-            train_seed: The random seed for train.
-            test_seed: The random seed for test.
-            device: The device to run the agent on. This is either "cpu" or "cuda".
-            default_root_dir: default root directory to store the results.
+            max_epsilon: The maximum epsilon. min_epsilon: The minimum epsilon. gamma:
+            The discount factor. capacity: The capacity of each human-like memory
+            systems. pretrain_semantic: Whether or not to pretrain the semantic memory
+            system. nn_params: The parameters for the function approximator. run_test:
+            Whether or not to run test. num_samples_for_results: The number of samples
+            to validate / test the agent. plotting_interval: The interval to plot the
+            results. train_seed: The random seed for train. test_seed: The random seed
+            for test. device: The device to run the agent on. This is either "cpu" or
+            "cuda". default_root_dir: default root directory to store the results.
 
         """
         all_params = deepcopy(locals())
@@ -138,10 +133,9 @@ class PPOAgent(HandcraftedAgent):
             self.env.unwrapped.total_maximum_episode_rewards
         )
 
-        assert self.num_rollouts % self.num_episodes == 0
-        assert (
-            self.num_episodes / self.num_rollouts * self.num_steps_in_episode
-        ).is_integer()
+        assert (self.num_rollouts % self.num_episodes) == 0 or (
+            self.num_episodes % self.num_rollouts
+        ) == 0
 
         self.num_steps_per_rollout = int(
             self.num_episodes / self.num_rollouts * self.num_steps_in_episode
@@ -185,17 +179,97 @@ class PPOAgent(HandcraftedAgent):
         self.actor_probs_all = {"train": [], "val": [], "test": []}
         self.critic_values_all = {"train": [], "val": [], "test": []}
 
+    def init_memory_env_reset_encode_observation(self) -> list:
+        """Init memory systems, reset environment, and encode observation.
+
+        Returns:
+            question: [head, relation, ?, current_time]
+        """
+        self.init_memory_systems()
+        (observation, question), info = self.env.reset()
+        encode_observation(self.memory_systems, observation)
+
+        return question
+
+    def step(
+        self,
+        question: list,
+        is_train_val_test: bool,
+        states_buffer: list | None = None,
+        actions_buffer: list | None = None,
+        values_buffer: list | None = None,
+        log_probs_buffer: list | None = None,
+        append_states_actions_probs_values: bool = False,
+        save_states: bool = False,
+    ) -> tuple[int, bool, list[str]]:
+        """Interact with the actual gymnasium environment by taking a step.
+
+        Args:
+            question: The question from the environment.
+            is_train_val_test: Whether the agent is in train, validation, or test mode.
+
+
+
+        Returns:
+            reward: reward from the gynmasium environment
+            done: whether the environment terminated or not
+            question: the question from the environment
+
+        """
+        state = self.memory_systems.return_as_a_dict_list()
+        action, actor_probs, critic_value = select_action(
+            actor=self.actor,
+            critic=self.critic,
+            state=state,
+            is_test=(is_train_val_test in ["val", "test"]),
+            states=states_buffer,
+            actions=actions_buffer,
+            values=values_buffer,
+            log_probs=log_probs_buffer,
+        )
+
+        if append_states_actions_probs_values:
+            if save_states:
+                # state is a list, which is a mutable object. So, we need to deepcopy
+                # it.
+                self.states_all[is_train_val_test].append(deepcopy(state))
+            else:
+                self.states_all[is_train_val_test].append(None)
+            self.actions_all[is_train_val_test].append(action)
+            self.actor_probs_all[is_train_val_test].append(actor_probs)
+            self.critic_values_all[is_train_val_test].append(critic_value)
+
+        manage_memory(
+            self.memory_systems,
+            self.action2str[action],
+            split_possessive=True,
+        )
+
+        answer = str(
+            answer_question(self.memory_systems, "episodic_semantic", question)
+        )
+
+        (
+            (observation, question),
+            reward,
+            done,
+            truncated,
+            info,
+        ) = self.env.step(answer)
+
+        encode_observation(self.memory_systems, observation)
+        done = done or truncated
+
+        return reward, done, question
+
     def create_empty_rollout_buffer(self) -> tuple[list, list, list, list, list, list]:
         """Create empty buffer for training.
 
         Make sure to call this before and after each rollout.
 
         Returns:
-            states_buffer: The states.
-            actions_buffer: The actions.
-            rewards_buffer: The rewards.
-            values_buffer: The values.
-            masks_buffer: The masks.
+            states_buffer: The states. actions_buffer: The actions. rewards_buffer: The
+            rewards. values_buffer: The values. masks_buffer: The masks.
             log_probs_buffer: The log probabilities.
         """
         # memory for training
@@ -220,15 +294,13 @@ class PPOAgent(HandcraftedAgent):
 
         self.num_validation = 0
 
-        self.init_memory_systems()
-        (observation, question), info = self.env.reset()
-        encode_observation(self.memory_systems, observation)
+        question = self.init_memory_env_reset_encode_observation()
 
         new_episode_starts = True
         score = 0
         episode_idx = 0
 
-        for rollout_idx in tqdm(range(self.num_rollouts)):
+        for _ in tqdm(range(self.num_rollouts)):
             (
                 states_buffer,
                 actions_buffer,
@@ -241,53 +313,24 @@ class PPOAgent(HandcraftedAgent):
             for _ in range(self.num_steps_per_rollout):
 
                 if new_episode_starts:
-                    self.init_memory_systems()
-                    (observation, question), info = self.env.reset()
-                    encode_observation(self.memory_systems, observation)
+                    question = self.init_memory_env_reset_encode_observation()
 
-                state = self.memory_systems.return_as_a_dict_list()
-                action, actor_probs, critic_value = select_action(
-                    actor=self.actor,
-                    critic=self.critic,
-                    state=state,
-                    is_test=False,
-                    states=states_buffer,
-                    actions=actions_buffer,
-                    values=values_buffer,
-                    log_probs=log_probs_buffer,
+                reward, done, question = self.step(
+                    question=question,
+                    is_train_val_test="train",
+                    states_buffer=states_buffer,
+                    actions_buffer=actions_buffer,
+                    values_buffer=values_buffer,
+                    log_probs_buffer=log_probs_buffer,
+                    append_states_actions_probs_values=True,
+                    save_states=False,
                 )
-
-                self.states_all["train"].append(None)  # this is a placeholder.
-                self.actions_all["train"].append(action)
-                self.actor_probs_all["train"].append(actor_probs)
-                self.critic_values_all["train"].append(critic_value)
-
-                manage_memory(
-                    self.memory_systems,
-                    self.action2str[action],
-                    split_possessive=True,
-                )
-
-                answer = str(
-                    answer_question(self.memory_systems, "episodic_semantic", question)
-                )
-
-                (
-                    (observation, question),
-                    reward,
-                    done,
-                    truncated,
-                    info,
-                ) = self.env.step(answer)
                 score += reward
 
-                encode_observation(self.memory_systems, observation)
-                done = done or truncated
                 next_state = self.memory_systems.return_as_a_dict_list()
 
                 reward = np.reshape(reward, (1, -1)).astype(np.float64)
                 done = np.reshape(done, (1, -1))
-
                 rewards_buffer.append(torch.FloatTensor(reward).to(self.device))
                 masks_buffer.append(torch.FloatTensor(1 - done).to(self.device))
 
@@ -348,53 +391,28 @@ class PPOAgent(HandcraftedAgent):
         self.critic.eval()
 
         scores = []
-
         for idx in range(self.num_samples_for_results):
-            self.init_memory_systems()
-            (observation, question), info = self.env.reset()
-            encode_observation(self.memory_systems, observation)
+            question = self.init_memory_env_reset_encode_observation()
 
             done = False
             score = 0
             while not done:
                 if idx == self.num_samples_for_results - 1:
-                    save_results = True
+                    append_results = True
                 else:
-                    save_results = False
+                    append_results = False
 
-                state = self.memory_systems.return_as_a_dict_list()
-
-                action, actor_probs, critic_value = select_action(
-                    actor=self.actor,
-                    critic=self.critic,
-                    state=state,
-                    is_test=True,
+                reward, done, question = self.step(
+                    question=question,
+                    is_train_val_test="val",
+                    states_buffer=None,
+                    actions_buffer=None,
+                    values_buffer=None,
+                    log_probs_buffer=None,
+                    append_states_actions_probs_values=append_results,
+                    save_states=True,
                 )
-                if save_results:
-                    self.states_all["val"].append(deepcopy(state))
-                    self.actions_all["val"].append(action)
-                    self.actor_probs_all["val"].append(actor_probs)
-                    self.critic_values_all["val"].append(critic_value)
-
-                manage_memory(
-                    self.memory_systems,
-                    self.action2str[action],
-                    split_possessive=True,
-                )
-
-                answer = str(
-                    answer_question(self.memory_systems, "episodic_semantic", question)
-                )
-                (
-                    (observation, question),
-                    reward,
-                    done,
-                    truncated,
-                    info,
-                ) = self.env.step(answer)
                 score += reward
-                done = done or truncated
-                encode_observation(self.memory_systems, observation)
 
             scores.append(score)
 
@@ -453,49 +471,27 @@ class PPOAgent(HandcraftedAgent):
 
         scores = []
         for idx in range(self.num_samples_for_results):
-            self.init_memory_systems()
-            (observation, question), info = self.env.reset()
-            encode_observation(self.memory_systems, observation)
+            question = self.init_memory_env_reset_encode_observation()
 
             done = False
             score = 0
             while not done:
                 if idx == self.num_samples_for_results - 1:
-                    save_results = True
+                    append_results = True
                 else:
-                    save_results = False
+                    append_results = False
 
-                state = self.memory_systems.return_as_a_dict_list()
-
-                action, actor_probs, critic_value = select_action(
-                    actor=self.actor, critic=self.critic, state=state, is_test=True
+                reward, done, question = self.step(
+                    question=question,
+                    is_train_val_test="test",
+                    states_buffer=None,
+                    actions_buffer=None,
+                    values_buffer=None,
+                    log_probs_buffer=None,
+                    append_states_actions_probs_values=append_results,
+                    save_states=True,
                 )
-                if save_results:
-                    self.states_all["test"].append(deepcopy(state))
-                    self.actions_all["test"].append(action)
-                    self.actor_probs_all["test"].append(actor_probs)
-                    self.critic_values_all["test"].append(critic_value)
-
-                manage_memory(
-                    self.memory_systems,
-                    self.action2str[action],
-                    split_possessive=True,
-                )
-
-                answer = str(
-                    answer_question(self.memory_systems, "episodic_semantic", question)
-                )
-                (
-                    (observation, question),
-                    reward,
-                    done,
-                    truncated,
-                    info,
-                ) = self.env.step(answer)
                 score += reward
-
-                encode_observation(self.memory_systems, observation)
-                done = done or truncated
 
             scores.append(score)
 
@@ -528,16 +524,12 @@ class PPOAgent(HandcraftedAgent):
 
         Args:
             to_plot: what to plot:
-                all: everything
-                actor_loss: actor loss
-                critic_loss: critic loss
-                scores: train, val, and test scores
-                actor_probs_train: actor probabilities for training
-                actor_probs_val: actor probabilities for validation
-                actor_probs_test: actor probabilities for test
-                critic_values_train: critic values for training
-                critic_values_val: critic values for validation
-                critic_values_test: critic values for test
+                all: everything actor_loss: actor loss critic_loss: critic loss scores:
+                train, val, and test scores actor_probs_train: actor probabilities for
+                training actor_probs_val: actor probabilities for validation
+                actor_probs_test: actor probabilities for test critic_values_train:
+                critic values for training critic_values_val: critic values for
+                validation critic_values_test: critic values for test
 
         """
         plot_results(
